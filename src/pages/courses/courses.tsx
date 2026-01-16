c3eee9eda07349a4582651b843cd1c95558a19b8
Kiril Chervoniak <42374203+kirilche@users.noreply.github.com>
2026-01-16 20:26:16 +0200

    Fix typo in heading from 'Cources' to 'Courses'

--- src/pages/courses/courses.tsx
+++ src/pages/courses/courses.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Courses = () => (
   <div className="container">
-    <h1 className='text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl'>Cources page</h1>
+    <h1 className='text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl'>Courses page</h1>
   </div>
 );
 

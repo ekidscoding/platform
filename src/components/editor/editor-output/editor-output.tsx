2ef34bae72bf1d78b494fa695e1c01bc3dd6d342
Igor Ovcharenko <igor_ovcharenko@epam.com>
2026-02-12 14:35:07 +0200

    Fixed auto scroll and height of output

--- src/components/editor/editor-output/editor-output.tsx
+++ src/components/editor/editor-output/editor-output.tsx
@@ -6,7 +6,7 @@ type EditorOutputProps = {
 }
 
 const EditorOutput = ({ output = '', error = '' }: EditorOutputProps) => (
-  <div className="editor-output-wrapper bg-background w-full min-h-[100px] border">
+  <div className="editor-output-wrapper bg-background w-full min-h-[100px] max-h-[18vh] border overflow-y-auto">
       <div className="bg-gray-400 py-1 px-2.5">Output:</div>
       <div className="p-2.5">
           <pre>

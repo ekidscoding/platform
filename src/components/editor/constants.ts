e12b5d107f9114cd18647e21801611865f7376d0
Igor Ovcharenko <igor_ovcharenko@epam.com>
2026-02-15 14:11:07 +0200

    Fixed resetting editor, submit button hover

--- src/components/editor/constants.ts
+++ src/components/editor/constants.ts
@@ -1,4 +1,6 @@
 export const DEFAULT_EDITOR_TEXT = '// Your code here';
+export const DEFAULT_OUTPUT_TEXT = '';
+export const DEFAULT_OUTPUT_ERROR = '';
 
 export const EDITOR_THEME = {
     DARK: 'Sunburst',

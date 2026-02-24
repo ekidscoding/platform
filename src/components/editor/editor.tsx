7ca4c43d7f8decac6f84cd0dcfb7691a38921ca6
Igor Ovcharenko <igor_ovcharenko@epam.com>
2026-02-25 00:05:22 +0200

    Added loading code samples, adjusting editor designs

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -8,27 +8,36 @@ import EditorInput from "./editor-input";
 import EditorOutput from "./editor-output";
 import { DEFAULT_EDITOR_TEXT, DEFAULT_OUTPUT_TEXT, DEFAULT_OUTPUT_ERROR } from "./constants";
 
-const Editor = () => {
-    const [editorCode, setEditorCode] = useState<string>(DEFAULT_EDITOR_TEXT);
+type EditorProps = {
+    codeSample?: string | null;
+    hasNavigation?: boolean;
+};
+
+const Editor = ({
+    codeSample = DEFAULT_EDITOR_TEXT,
+    hasNavigation = false,
+}: EditorProps) => {
+    const [editorCode, setEditorCode] = useState<string | null>(codeSample);
     const [outputCode, setOutputCode] = useState<string>(DEFAULT_OUTPUT_TEXT);
     const [outputError, setOutputError] = useState<string>(DEFAULT_OUTPUT_ERROR);
     const { runPython, stdout, stderr, isLoading, isRunning, sendInput, prompt } = usePython();
     const { state } = useSidebar();
 
     const executeHandler = useCallback(async () => {
-        await runPython(editorCode);
+        await runPython(editorCode ?? '');
     }, [editorCode]);
 
     const editorContainerClassList = classNames(
-        "editor-container w-full max-w-[100%] h-svh pt-16 overflow-hidden",
+        "editor-container w-full max-w-[100%] h-svh pt-[84px] overflow-hidden",
         "transition-[max-width] duration-500 ease-in-out",
         {
             "max-w-[50%]": state === 'expanded',
+            "md:pt-[126px]": hasNavigation,
         },
     );
 
     const handleResetCode = () => {
-        setEditorCode(DEFAULT_EDITOR_TEXT);
+        setEditorCode(codeSample);
         setOutputCode(DEFAULT_OUTPUT_TEXT);
         setOutputError(DEFAULT_OUTPUT_ERROR);
     };
@@ -40,6 +49,10 @@ const Editor = () => {
     useEffect(() => {
         setOutputError(stderr);
     }, [stderr]);
+
+    useEffect(() => {
+        setEditorCode(codeSample);
+    }, [codeSample]);
 
     useEffect(() => {
         if (prompt) {

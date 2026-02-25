26d95f8486c6409c7b46ce309abf16ae8b9ec0c8
Kiril Chervoniak <42374203+kirilche@users.noreply.github.com>
2026-02-25 11:18:55 +0000

    add UUIDs

---
+++ public/python-solutions/c73cc375-ff96-4d04-b269-1ea87e2c10b4.py
@@ -0,0 +1,8 @@
+def isOdd(number):
+    # Return whether number mod 2 is 1:
+    return number % 2 == 1
+
+
+def isEven(number):
+    # Return whether number mod 2 is 0:
+    return number % 2 == 0
---
+++ public/python-templates/c73cc375-ff96-4d04-b269-1ea87e2c10b4.py
@@ -0,0 +1,8 @@
+def isOdd(number):
+    # Return whether number mod 2 is 1:
+    return ____ % 2 == ____
+
+
+def isEven(number):
+    # Return whether number mod 2 is 0:
+    return ____ % 2 == ____

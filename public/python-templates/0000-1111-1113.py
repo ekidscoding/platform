b7d4ef3e98f3932b4bf95b9093b3265bf8215600
Kiril Chervoniak <kiril.chervoniak@medecision.com>
2026-02-25 10:27:10 +0200

    add third lesson, center buttons

---
+++ public/python-templates/0000-1111-1113.py
@@ -0,0 +1,9 @@
+delivery_times = [19, 25, 35, 40, 28, 32, 29, 31]
+
+# Find the average delivery time
+average_time = sum(delivery_times) / len(delivery_times)
+
+# Round the average delivery time to two decimal places
+average_rounded = round(average_time, 2)
+
+print("Average delivery time:", average_rounded)
--- src/components/lessons/lesson/lesson.tsx
+++ src/components/lessons/lesson/lesson.tsx
@@ -15,71 +15,71 @@ import useFetchCodeSample from "@/hooks/api/use-fetch-code-sample";
 import SidebarTrigger from "./sidebar-trigger";
 
 const Lesson = () => {
-  const { id } = useParams();
-  const [searchParams] = useSearchParams();
-  const courseId = searchParams.get('course');
+    const { id } = useParams();
+    const [searchParams] = useSearchParams();
+    const courseId = searchParams.get('course');
 
-  const { data, error, isFetching, isLoading } = useFetchLesson(id);
-  const { data: courseData } = useFetchCourse(courseId ?? '');
-  const { data: codeSample } = useFetchCodeSample(id);
-  const prevLesson = courseData?.tableOfContents.find(lesson => lesson.id === id)?.prevLesson;
-  const nextLesson = courseData?.tableOfContents.find(lesson => lesson.id === id)?.nextLesson;
-  const isLoadingState = isLoading || isFetching;
+    const { data, error, isFetching, isLoading } = useFetchLesson(id);
+    const { data: courseData } = useFetchCourse(courseId ?? '');
+    const { data: codeSample } = useFetchCodeSample(id);
+    const prevLesson = courseData?.tableOfContents.find(lesson => lesson.id === id)?.prevLesson;
+    const nextLesson = courseData?.tableOfContents.find(lesson => lesson.id === id)?.nextLesson;
+    const isLoadingState = isLoading || isFetching;
 
-  const lessonColumnClassList = classNames(
-      "h-full overflow-y-auto py-[84px] px-[15px]",
-      {
-          "md:pt-[126px]": nextLesson || prevLesson,
-      }
-  )
+    const lessonColumnClassList = classNames(
+        "h-full overflow-y-auto py-[84px] px-[15px]",
+        {
+            "md:pt-[126px]": nextLesson || prevLesson,
+        }
+    )
 
-  const LessonTemplate = () => (
-      <>
-          {isLoadingState ? (
-              <>
-                  <Spinner /> Loading...
-              </>
-          ) : error ? (
-              <div className='text-destructive'>Error: {error.message}</div>
-          ) : (
-              <Markdown>{data?.data ?? ''}</Markdown>
-          )}
-      </>
-  );
+    const LessonTemplate = () => (
+        <>
+            {isLoadingState ? (
+                <>
+                    <Spinner /> Loading...
+                </>
+            ) : error ? (
+                <div className='text-destructive'>Error: {error.message}</div>
+            ) : (
+                <Markdown>{data?.data ?? ''}</Markdown>
+            )}
+        </>
+    );
 
-  return (
-      <SidebarProvider className="block md:flex">
-          <div className="md:hidden w-full">
-              <LessonTemplate />
-          </div>
-          <div className="hidden md:block">
-              <Sidebar>
-                  <div className={lessonColumnClassList}>
-                      <LessonTemplate />
-                  </div>
-                  <SidebarTrigger className="absolute right-[-52px] top-[63px]" />
-              </Sidebar>
-          </div>
-          <Editor codeSample={codeSample?.data} hasNavigation={Boolean(nextLesson || prevLesson)} />
-          {(nextLesson || prevLesson) && (
-              <div
-                  className="fixed w-full flex justify-between left-0 top-[64px] bg-background shadow-[4px_5px_6px_0px_rgba(0,_0,_0,_0.1)] p-[15px] z-51">
-                  {prevLesson && <Button variant="outline" size="sm">
-                      <ArrowLeft/>
-                      <Link to={`/lessons/${prevLesson}?course=${courseId}`}>
-                          Previous Lesson
-                      </Link>
-                  </Button>}
-                  {nextLesson && <Button className="pr-auto" variant="outline" size="sm">
-                      <Link to={`/lessons/${nextLesson}?course=${courseId}`}>
-                          Next Lesson
-                      </Link>
-                      <ArrowRight/>
-                  </Button>}
-              </div>
-          )}
-      </SidebarProvider>
-  );
+    return (
+        <SidebarProvider className="block md:flex">
+            <div className="md:hidden w-full">
+                <LessonTemplate />
+            </div>
+            <div className="hidden md:block">
+                <Sidebar>
+                    <div className={lessonColumnClassList}>
+                        <LessonTemplate />
+                    </div>
+                    <SidebarTrigger className="absolute right-[-52px] top-[63px]" />
+                </Sidebar>
+            </div>
+            <Editor codeSample={codeSample?.data} hasNavigation={Boolean(nextLesson || prevLesson)} />
+            {(nextLesson || prevLesson) && (
+                <div
+                    className="content-center fixed w-full flex justify-between left-0 top-[64px] bg-background shadow-[4px_5px_6px_0px_rgba(0,_0,_0,_0.1)] p-[15px] z-51">
+                    {prevLesson && <Button variant="outline" size="sm">
+                        <ArrowLeft/>
+                        <Link to={`/lessons/${prevLesson}?course=${courseId}`}>
+                            Previous Lesson
+                        </Link>
+                    </Button>}
+                    {nextLesson && <Button className="pr-auto" variant="outline" size="sm">
+                        <Link to={`/lessons/${nextLesson}?course=${courseId}`}>
+                            Next Lesson
+                        </Link>
+                        <ArrowRight/>
+                    </Button>}
+                </div>
+            )}
+        </SidebarProvider>
+    );
 };
 
 export default Lesson;
